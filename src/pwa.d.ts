41fd7c28a1735de6baf8e58fb4075bb7857db80c
famlyman <burkecustomcomputers@gmail.com>
2026-04-26 14:16:27 -0400

    OneSignal SDK Worker

--- src/pwa.d.ts
+++ src/pwa.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite-plugin-pwa/client" />
+
+declare global {
+  interface Window {
+    OneSignal: {
+      push: (cb: () => void) => void;
+      init: (config: { appId: string; allowLocalhostAsSecureOrigin?: boolean; autoResend?: boolean }) => void;
+    };
+  }
+}
